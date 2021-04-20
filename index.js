e8b48c013cde475b18e8ec91dfa51008219e5cea
MAshroorsakib <sakibuap01@gmail.com>
2021-04-20 22:47:52 +0800

    server code

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const collection = client.db("Evaly").collection("evalyproduct");
   const bookingcollection = client.db("Evaly").collection("bookingproduct");
-  
+  const customerreview = client.db("Evaly").collection("review");
+  const admincollection = client.db("Evaly").collection("admin");
+
   app.post('/addevent',(req,res)=>{
     const newevent=req.body;
     console.log("adding new eventg",newevent)
@@ -33,12 +35,48 @@ client.connect(err => {
     })
 })
 
+
+app.post('/addadmin',(req,res)=>{
+  const newadmin=req.body;
+  console.log("adding new admin",newadmin)
+  admincollection.insertOne(newadmin)
+  .then(result=>{
+    console.log("inserted count",result.insertedCount)
+    res.send(result.insertedCount>0)
+  })
+})
+
+app.post('/addreview',(req,res)=>{
+  const newevent=req.body;
+  console.log("adding new review",newevent)
+  customerreview.insertOne(newevent)
+  .then(result=>{
+    console.log("inserted count",result.insertedCount)
+    res.send(result.insertedCount>0)
+  })
+})
+
+
 app.get('/showdata',(req,res)=>{
     collection.find()
     .toArray((err,items)=>{
       res.send(items)
     })
   })
+
+  app.get('/showalldata',(req,res)=>{
+    bookingcollection.find()
+    .toArray((err,items)=>{
+      res.send(items)
+    })
+  })
+
+  app.get('/showreview',(req,res)=>{
+    customerreview.find()
+    .toArray((err,items)=>{
+      res.send(items)
+    })
+  }) 
 
 app.get('/singleproduct/:id',(req,res)=>{
   collection.find({_id: ObjectID(req.params.id)})
@@ -47,6 +85,14 @@ app.get('/singleproduct/:id',(req,res)=>{
   })
 })  
 
+app.post('/isadmin',(req,res)=>{
+  const email=req.body.email
+  admincollection.find({Email:email})
+  .toArray((err,admin)=>{
+    res.send(admin.length>0)
+  })
+}) 
+
 app.get('/booking/:email',(req,res)=>{
   bookingcollection.find({email: req.params.email})
   .toArray((err,document)=>{
@@ -54,6 +100,17 @@ app.get('/booking/:email',(req,res)=>{
    //console.log(document)
   })
 })
+
+
+app.patch('/update/:id',(req,res)=>{
+  bookingcollection.updateOne({_id: ObjectId(req.params.id)},
+   {
+     $set:{status:req.body.status}
+   })
+   .then(result=>{
+     res.send(result.modifiedCount>0)
+   })
+ })
 
 app.post('/bookingdetails',(req,res)=>{
     const newbooking=req.body;
